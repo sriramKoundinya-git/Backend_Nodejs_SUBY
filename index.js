70e391ab4380319bf4a9613c200de707d48247a5
sriram2021-2024 <pamarthisuryavenkatasriram@gmail.com>
2026-02-10 11:58:13 +0530

    cors dependencie added

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const vendorRoutes = require('./routes/vendorRoute');
 const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 4000;
 dotEnv.config();
 mongoose.connect(process.env.MONGO_URI)
